08f882022b3044977d69960c782e4af956353883
knightza123 <knightza123@hotmail.com>
2018-03-10 19:52:00 +0700

    fix slide show

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -9,25 +9,25 @@ class Footer extends Component {
                 <div className="footerbody footer">
                     <Link to="/">
                         <div className="footericon">
-                            <Icon type="appstore"/>
+                            <Icon type="appstore" style={{fontSize:16}}/>
                             <h2>Home</h2>
 
                         </div>
                     </Link>
                     <Link to="/express">
                         <div className="footericon">
-                            <Icon type="eye"/>
+                            <Icon type="eye" style={{fontSize:16}}/>
                             <h2>Express</h2>
                         </div>
                     </Link>
                     <Link to="/myplan">
                         <div className="footericon">
-                            <Icon type="rocket"/>
+                            <Icon type="rocket" style={{fontSize:16}}/>
                             <h2>My Plan</h2>
                         </div>
                     </Link>
                     <div className="footericon">
-                        <Icon type="rocket"/>
+                        <Icon type="rocket" style={{fontSize:16}}/>
                         <h2>Profile</h2>
                     </div>
 
