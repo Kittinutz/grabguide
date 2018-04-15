0368ead4642853a65007b924b7b08a1b1b380792
kittinutz <knightza94@gmail.com>
2018-04-15 16:26:16 +0700

    my plan build new contructure

--- src/container/Myplan/HOCMyplan.js
+++ src/container/Myplan/HOCMyplan.js
@@ -2,28 +2,43 @@ import {withState, compose, withHandlers, lifecycle} from 'recompose'
 import {connect} from 'react-redux'
 import {bindActionCreators} from "redux";
 import {fetchmessage} from '../../action/Authentication';
+import {GetActivitiesAPi,querybooking} from "../../action/index";
+
+const plus = withState("plus","setplus",false);
+
 const Lifecycle = lifecycle(
   {
     componentDidMount() {
       console.log(this.props);
       // const {id} = this.props.match.params;
       if (!this.props.authentication.isAuth) {
-        return this.props.history.push(`/facebook/`);
+        return this.props.history.push(`/login/facebook`);
       } else {
-        return this.props.fetchmessage();
+        this.props.GetActivitiesAPi();
+        return this.props.querybooking();
+        
       }
     }
+    
   }
 );
-const mapStateToProps = ({bookingdetail, authentication}) => {
+const handleEvent = withHandlers({
+  HandleClickIcon: props => event => {
+    props.history.push('/createtrip/')
+  }
+})
+const mapStateToProps = ({bookingdetail, authentication,mytrip}) => {
   return {
-    bookingdetail,
-    authentication
+    authentication,
+    mytrip
   }
 }
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators({fetchmessage, booking}, dispatch);
+  return bindActionCreators({fetchmessage,GetActivitiesAPi,querybooking}, dispatch);
 };
 export default compose(
+  connect(mapStateToProps,mapDispatchToProps),
   Lifecycle,
+  handleEvent,
+  plus
 )
--- src/container/Myplan/Myplan.js
+++ src/container/Myplan/Myplan.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import enhance from './HOCMyplan'
 import Footer from '../../component/Footer/index';
 import Header from '../../component/Header/Header';
 import _ from 'lodash';
@@ -16,96 +17,93 @@ const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
 const { Meta } = Card;
 
-function handleChange(value) {
-    console.log(`Selected: ${value}`);
+
+const Myplan =(props)=>{
+    console.log(props)
+    return (
+      <div>
+          Helloworld
+      </div>
+    )
 }
+export default enhance(Myplan)
 
-function onChange(date, dateString) {
-    console.log(date, dateString);
-}
-
-class Myplan extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            place: [],
-            plus: false,
-        }
-    }
-
-    componentWillMount() {
-        this.props.GetActivitiesAPi();
-        this.props.querybooking();
-
-    };
-
-
-    HandleClickIcon = () => {
-        this.props.history.push('/createtrip/')
-    };
-
-    render() {
-        console.log(this.props);
-        const place = [];
-        const activities = [];
-        // const { getFieldDecorator } = this.props.form;
-        const formItemLayout = {
-            labelCol: {span: 6},
-            wrapperCol: {span: 14},
-            marginLeft: 20,
-        };
-        if (this.props.mytrip) {
-            return (
-
-                <div>
-                    <Header icon="plus-circle-o" handleClick={this.HandleClickIcon}/>
-                    <div className="content">
-                        {
-                            _.map(this.props.mytrip,data=>{
-                                console.log(data)
-                                return(
-                                    <Card
-                                        key={data.id}
-                                        style={{ width: "100%" ,marginTop:"10%" }}
-                                        cover={<img alt={data.name} src={data.trip.image} />}
-                                        actions={[<Link to={`/myplan/${data.id}`}><Icon type="book" /></Link>, <Icon type="edit" />, <Icon type="ellipsis" />]}
-                                    >
-                                        <Meta
-                                            avatar={<Avatar src={data.trip.guide.image} />}
-                                            title={data.trip.name}
-                                            description={data.trip.description}
-                                        />
-                                    </Card>
-
-                                )
-                            })
-
-                        }
-                    </div>
-                    <Footer/>
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <Header icon="plus-circle-o" handleClick={this.HandleClickIcon}/>
-                    <div className="content">
-                        <div className="container">
-
-                        </div>
-                    </div>
-                    <Footer/>
-                </div>
-            )
-        }
-    }
-}
-
-function mapStateToProps({place, activities, mytrip}) {
-    return {
-        mytrip
-    }
-
-}
-
-export default connect(mapStateToProps, actions)(Myplan);
+// class Myplan extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             place: [],
+//             plus: false,
+//         }
+//     }
+//
+//     componentWillMount() {
+//     =
+//         this.props.GetActivitiesAPi();
+//         this.props.querybooking();
+//
+//     };
+//
+//
+//     HandleClickIcon = () => {
+//         this.props.history.push('/createtrip/')
+//     };
+//
+//     render() {
+//         console.log(this.props);
+//
+//         if (this.props.mytrip) {
+//             return (
+//
+//                 <div>
+//                     <Header icon="plus-circle-o" handleClick={this.HandleClickIcon}/>
+//                     <div className="content">
+//                         {
+//                             _.map(this.props.mytrip,data=>{
+//                                 console.log(data)
+//                                 return(
+//                                     <Card
+//                                         key={data.id}
+//                                         style={{ width: "100%" ,marginTop:"10%" }}
+//                                         cover={<img alt={data.name} src={data.trip.image} />}
+//                                         actions={[<Link to={`/myplan/${data.id}`}><Icon type="book" /></Link>, <Icon type="edit" />, <Icon type="ellipsis" />]}
+//                                     >
+//                                         <Meta
+//                                             avatar={<Avatar src={data.trip.guide.image} />}
+//                                             title={data.trip.name}
+//                                             description={data.trip.description}
+//                                         />
+//                                     </Card>
+//
+//                                 )
+//                             })
+//
+//                         }
+//                     </div>
+//                     <Footer/>
+//                 </div>
+//             )
+//         } else {
+//             return (
+//                 <div>
+//                     <Header icon="plus-circle-o" handleClick={this.HandleClickIcon}/>
+//                     <div className="content">
+//                         <div className="container">
+//
+//                         </div>
+//                     </div>
+//                     <Footer/>
+//                 </div>
+//             )
+//         }
+//     }
+// }
+//
+// function mapStateToProps({place, activities, mytrip}) {
+//     return {
+//         mytrip
+//     }
+//
+// }
+//
+// export default connect(mapStateToProps, actions)(Myplan);
